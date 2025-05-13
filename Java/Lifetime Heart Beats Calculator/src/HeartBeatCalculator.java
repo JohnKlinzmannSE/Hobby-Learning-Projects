38f2960a515d4ea43afccf8908b2897ca1b2e7ee
OB1CodeOB <102632526+OB1CodeOB@users.noreply.github.com>
2025-05-13 14:48:20 -0700

    Created class for the calculator and added variables.
    Initialized avgBeatsPerMinute variable to equal 72.

---
+++ Java/Lifetime Heart Beats Calculator/src/HeartBeatCalculator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HeartBeatCalculator {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner (System.in);
+        int userAgeYears;
+        int userAgeMonths;
+        int userAgeDays;
+        int userAgeHours;
+        int userAgeMinutes;
+        int userAgeSeconds;
+        int totalHeartBeats;
+        int avgHeartBeatsPerMinute = 72;
+
+    }
+}
