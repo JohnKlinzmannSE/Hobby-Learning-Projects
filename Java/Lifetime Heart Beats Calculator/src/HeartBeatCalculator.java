f1a1a22383a0e5a196bdc8cbbe0b2f3b686396b3
OB1CodeOB <102632526+OB1CodeOB@users.noreply.github.com>
2025-05-13 15:03:11 -0700

    Added calculations for user age in hours, minutes, and seconds.
    Added calculation for total heart beats per minute for the user's lifetime.

--- Java/Lifetime Heart Beats Calculator/src/HeartBeatCalculator.java
+++ Java/Lifetime Heart Beats Calculator/src/HeartBeatCalculator.java
@@ -17,13 +17,24 @@ public class HeartBeatCalculator {
         System.out.print("Please enter your current age in year.");
         userAgeYears = scnr.nextInt();
 
-//        Calculate Months
+//        Calculate Age in Months
         userAgeMonths = userAgeYears * 12;
 
         userAgeWeeks = userAgeYears * 52;
 
-//        Calculate Days
+//        Calculate Age in Days
         userAgeDays = userAgeYears * 365;
 
+//        Calculate Age in Hours
+        userAgeHours = userAgeDays * 24;
+
+//        Calculate Age in Minutes
+        userAgeMinutes = userAgeHours * 60;
+
+//        Calculate Age in Seconds
+        userAgeSeconds = userAgeMinutes * 60;
+
+//        Calculate Total Heart Beats
+        totalHeartBeats = userAgeMinutes * avgHeartBeatsPerMinute;
     }
 }
