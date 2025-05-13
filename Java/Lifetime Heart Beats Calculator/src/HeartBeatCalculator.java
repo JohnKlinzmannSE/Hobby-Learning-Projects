2811d95f3ebbff7d30054e9949dfd9edc68a99ea
OB1CodeOB <102632526+OB1CodeOB@users.noreply.github.com>
2025-05-13 14:56:40 -0700

    Updated variables to include months and weeks.
    Added calculations for user age in months, weeks, and days.

--- Java/Lifetime Heart Beats Calculator/src/HeartBeatCalculator.java
+++ Java/Lifetime Heart Beats Calculator/src/HeartBeatCalculator.java
@@ -5,6 +5,7 @@ public class HeartBeatCalculator {
         Scanner scnr = new Scanner (System.in);
         int userAgeYears;
         int userAgeMonths;
+        int userAgeWeeks;
         int userAgeDays;
         int userAgeHours;
         int userAgeMinutes;
@@ -16,7 +17,13 @@ public class HeartBeatCalculator {
         System.out.print("Please enter your current age in year.");
         userAgeYears = scnr.nextInt();
 
+//        Calculate Months
+        userAgeMonths = userAgeYears * 12;
 
+        userAgeWeeks = userAgeYears * 52;
+
+//        Calculate Days
+        userAgeDays = userAgeYears * 365;
 
     }
 }
